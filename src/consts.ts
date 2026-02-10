8ebf7b8c4fe40b9477f793bbcd03f8336afd01aa
GarethWRWinter <garethwrwinter@outlook.com>
2026-02-10 13:10:49 +0000

    Port Sa Calobra Sub-30 + set site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Roadbook of Cycling';
+export const SITE_DESCRIPTION = 'Performance, craft, and the quiet obsession of getting better.';
